c17c4406900a614762959b334fa08b029f4b11fd
frankMnjoroge <frankmugo@gmail.com>
2024-02-01 13:24:02 -0800

    updated

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,7 +7,7 @@
  * Return: size of the tree
  *         0 if tree is NULL
  */
-size_t bin_tr_size(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
