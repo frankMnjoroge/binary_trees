9eaad7e4b9286261bbcafb114ab4e88c953878df
root <gicherushadrack@gmail.com>
2024-02-01 14:17:20 -0800

    24

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,16 +1,37 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: tree to measure the size of
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
+ * @tree: a pointer to the root node of the tree to check
  *
- * Return: size of the tree
- *         0 if tree is NULL
+ * Return: 1 if tree is a valid BST
+ *         0 otherwise
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
+	return (btib_helper(tree, INT_MIN, INT_MAX));
+}
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+/**
+ * btib_helper - checks if a binary tree is a valid Binary Search Tree
+ * @tree: a pointer to the root node of the tree to check
+ * @min: Lower bound of checked nored
+ * @max: Upper bound of checked nodes
+ *
+ * Return: 1 if tree is a valid BST
+ *         0 otherwise
+ */
+int btib_helper(const binary_tree_t *tree, int min, int max)
+{
+	if (!tree)
+		return (1);
+
+	if (tree->n < min || tree->n > max)
+		return (0);
+
+	return (btib_helper(tree->left, min, tree->n - 1) &&
+		btib_helper(tree->right, tree->n + 1, max));
+	/* -1 and +1 stem from "There must be no duplicate values" req */
 }
